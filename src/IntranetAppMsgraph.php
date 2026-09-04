5d4aa2b45554174c347b693b18a62b1047742e42
aleex1848 <halycon01@gmail.com>
2026-09-04 11:07:24 +0200

    searchaction

--- src/IntranetAppMsgraph.php
+++ src/IntranetAppMsgraph.php
@@ -2,14 +2,16 @@
 
 namespace Hwkdo\IntranetAppMsgraph;
 
+use Hwkdo\IntranetAppBase\Data\SearchActionDefinition;
 use Hwkdo\IntranetAppBase\Interfaces\IntranetAppInterface;
 use Hwkdo\IntranetAppBase\Interfaces\ProvidesDashboardWidgetsInterface;
+use Hwkdo\IntranetAppBase\Interfaces\ProvidesSearchActionsInterface;
 use Hwkdo\IntranetAppMsgraph\Dashboard\MsgraphDashboardWidgetProvider;
 use Hwkdo\IntranetAppMsgraph\Data\AppSettings;
 use Hwkdo\IntranetAppMsgraph\Data\UserSettings;
 use Illuminate\Support\Collection;
 
-class IntranetAppMsgraph implements IntranetAppInterface, ProvidesDashboardWidgetsInterface
+class IntranetAppMsgraph implements IntranetAppInterface, ProvidesDashboardWidgetsInterface, ProvidesSearchActionsInterface
 {
     public static function app_name(): string
     {
@@ -55,6 +57,24 @@ class IntranetAppMsgraph implements IntranetAppInterface, ProvidesDashboardWidge
     {
         return [
             MsgraphDashboardWidgetProvider::class,
+        ];
+    }
+
+    public static function searchActions(): array
+    {
+        return [
+            new SearchActionDefinition(
+                key: 'msgraph.auslandszugriff',
+                title: 'Auslandszugriff',
+                keywords: ['auslandseinsatz', 'auslandseinsätze', 'ausland'],
+                routeName: 'apps.msgraph.auslandszugriff.index',
+                appIdentifier: self::identifier(),
+                appName: self::app_name(),
+                icon: 'globe-alt',
+                permission: 'see-app-msgraph',
+                subtitle: self::app_name(),
+                sort: 100,
+            ),
         ];
     }
 }
